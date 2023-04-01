56d0cd9053db9f1c75793fae617b1e949b4ff12f
lenovo <lenovo@DESKTOP-1I81RKP>
2023-04-01 13:35:57 +0530

    New Project

---
+++ ERP_StockAccounting/src/main/java/FreeOjt/App.java
@@ -0,0 +1,13 @@
+package FreeOjt;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
