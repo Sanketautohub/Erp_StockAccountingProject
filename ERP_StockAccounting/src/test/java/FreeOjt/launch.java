3ff63fd3bb17c59ae125a65e4e64f24086a68812
Sanketautohub <sanketmasurkar02@gmail.com>
2023-04-24 14:09:29 +0530

    First Commit

---
+++ ERP_StockAccounting/src/test/java/FreeOjt/launch.java
@@ -0,0 +1,23 @@
+package FreeOjt;
+
+public class launch {
+	int x=10;
+	
+	
+	public static void main (String [] args)
+{
+		
+  launch l = new launch();
+  l.sum();
+		  
+		
+		
+				
+}
+	
+	public void sum()
+	{
+		int c = x+10;
+		System.out.println(c);
+	}
+}
